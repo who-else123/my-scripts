241604f336a9b66e93e310be12099a23f0c3b905
who-else123 <99100316+who-else123@users.noreply.github.com>
2025-05-03 22:46:00 +0100

    Create honest-reviews.lua

---
+++ syncsaken/honest-reviews.lua
@@ -0,0 +1,62 @@
+return {
+	{
+		["quote"] = "i would literally sell my soul for syncsaken",
+		["user"] = "catpro_54"
+	},
+	{
+		["quote"] = "FANCY PURSE USERS SUCK",
+		["user"] = "veryretro"
+	},
+	{
+		["quote"] = "you greedy fancy purse user",
+		["user"] = "egg"
+	},
+	{
+		["quote"] = "on everyones soul we gooning to two time",
+		["user"] = "w1ndowp4in"
+	},
+	{
+		["quote"] = "i need to get my nightshade two time...",
+		["user"] = "blixer"
+	},
+	{
+		["quote"] = "RAHHHHHHHHHHHHHHH",
+		["user"] = "blixer"
+	},
+	{
+		["quote"] = "you fancy purse users and your greed disgusts me.",
+		["user"] = "veryretro"
+	},
+	{
+		["quote"] = "why wont the script execute?",
+		["user"] = "k1rby"
+	},
+	{
+		["quote"] = "Anyone got a free key frfr",
+		["user"] = "cakipopovic"
+	},
+	{
+		["quote"] = "My awp Expired today 😭",
+		["user"] = "skibiditoilet"
+	},
+	{
+		["quote"] = "I JUST GOT BANNED FOR 7993 DAYS",
+		["user"] = "mario"
+	},
+	{
+		["quote"] = "I mean unfuck you",
+		["user"] = "cakipopovic"
+	},
+	{
+		["quote"] = "rip kitteh",
+		["user"] = "kitteh"
+	},
+	{
+		["quote"] = "key systems are kaka",
+		["user"] = "craig"
+	},
+	{
+		["quote"] = "Remona you're t-t-the owner of elysian hub!?!?",
+		["user"] = "veryretro"
+	}
+}
